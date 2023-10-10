3fe8c174cf350779ef965eb76d7136ca9f67e06a
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-10-10 07:28:02 -0700

    update

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit command to exit the prog"""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the prog at EOF (Ctrl + D)"""
+        print()
+        return True
+
+    def emptyline(self):
+        """Do nothing on empty input line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
