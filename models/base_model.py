8bbe8883ca3f0f4159cb20fb31778e7294e13d12
Mohammed Amine Mounjid <medamine190@gmail.com>
2023-10-14 05:15:43 -0700

    update

--- models/base_model.py
+++ models/base_model.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """The class that defines all common attributes/methods for other classes"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -14,7 +14,7 @@ class BaseModel:
             **kwargs (dict): Key/value pairs of attributes.
         """
         timefrm = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
         if len(kwargs) != 0:
